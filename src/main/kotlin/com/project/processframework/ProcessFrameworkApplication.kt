a7926cd75282cf4a3efec76a619a7462318aabe0
Andreas Wassum <a.wassum@gmail.com>
2023-04-24 20:16:19 +0200

    Initial commit

---
+++ src/main/kotlin/com/project/processframework/ProcessFrameworkApplication.kt
@@ -0,0 +1,11 @@
+package com.project.processframework
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProcessFrameworkApplication
+
+fun main(args: Array<String>) {
+    runApplication<ProcessFrameworkApplication>(*args)
+}
